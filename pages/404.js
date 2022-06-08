45d296af9bd5d7b625e06c9fe33f03a39d1b189c
gkpk30 <guykoomer@hotmail.com>
2022-06-08 09:12:15 -0700

    wrapped text in brackets and qoutations because of escaping issues

--- pages/404.js
+++ pages/404.js
@@ -33,10 +33,10 @@ export default function NotFound() {
           <div className="max-w-7xl mx-auto px-4 py-16 text-center sm:px-6 sm:py-24 lg:px-8 lg:py-48">
             <p className="text-sm font-semibold text-black text-opacity-50 uppercase tracking-wide">404 error</p>
             <h1 className="mt-2 text-4xl font-extrabold text-white tracking-tight sm:text-5xl">
-              Uh oh! I think you’re lost.
+              {"Uh oh! I think you’re lost."}
             </h1>
             <p className="mt-2 text-lg font-medium text-black text-opacity-50">
-              It looks like the page you’re looking for doesn't exist.
+              {"It looks like the page you’re looking for doesn't exist."}
             </p>
             <div className="mt-6">
                 <Link href="/">
