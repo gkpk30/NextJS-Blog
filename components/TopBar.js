223aa8c48e8d3ecd7db5f81542210368bb7c79bd
gkpk30 <guykoomer@hotmail.com>
2022-06-17 21:39:38 -0700

    fixed topbar css border on current

--- components/TopBar.js
+++ components/TopBar.js
@@ -4,6 +4,7 @@ import { Disclosure, Menu, Transition } from "@headlessui/react";
 import {MenuIcon, XIcon } from "@heroicons/react/outline";
 import { MoonIcon, SunIcon } from "@heroicons/react/solid";
 import Link from "next/link";
+import { useRouter } from 'next/router'
 
 
 import { useTheme } from "next-themes";
@@ -13,6 +14,11 @@ function classNames(...classes) {
 }
 
 export default function TopBar() {
+  const router = useRouter()
+  console.log("router: " , router.pathname)
+  // const pathname = location.pathname
+  // console.log("pathname: ", pathname)
+  
   const { systemTheme, theme, setTheme } = useTheme();
   const [mounted, setMounted] = useState()
 
@@ -55,7 +61,7 @@ export default function TopBar() {
               <div className="relative flex justify-between h-16">
                 <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                   {/* Mobile menu button */}
-                  <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md dark:text-gray-50  text-gray-400 hover:text-gray-500  hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                  <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md dark:text-gray-50  text-gray-400 hover:text-gray-500  focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
                     <span className="sr-only">Open main menu</span>
                     {open ? (
                       <XIcon className="block h-6 w-6" aria-hidden="true" />
@@ -82,24 +88,31 @@ export default function TopBar() {
                     <div className="hidden sm:ml-12 sm:flex sm:space-x-6 ">
                       {/* Current: "border-indigo-500 text-gray-900", Default: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700" */}
                       <Link href="/">
-                        <a className="border-primary  text-gray-900 dark:text-gray-50 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium">
+                     
+                        <a  
+                        className={classNames(router.pathname === '/' ? "border-primary font-bold" : "border-transparent ", " hover:border-gray-300  text-gray-500 dark:text-gray-50 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium")}
+                        >
                           Home
                         </a>
                       </Link>
-                      <Link href="/blogs">
-                        <a className="border-transparent text-gray-500 dark:text-gray-50 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium">
+                      <Link href="/blogs"   >
+                        <a  
+                        className={classNames(router.pathname === '/blogs' ? "border-primary  font-bold" : "border-transparent ", " hover:border-gray-300 text-gray-500  dark:text-gray-50 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium")}
+                        >
                           Blogs
                         </a>
                       </Link>
                       <Link href="/gallery">
-                        <a className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 dark:text-gray-50 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium">
+                        <a 
+                         className={classNames(router.pathname === '/gallery' ? "border-primary font-bold" : "border-transparent ", " hover:border-gray-300 text-gray-500  dark:text-gray-50 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium")}
+                        >
                           Gallery
                         </a>
                       </Link>
                       <a
                         href="#"
-                        className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 dark:text-gray-50 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
-                      >
+                        className={classNames(router.pathname === '/projects' ? "border-primary font-bold" : "border-transparent ", " hover:border-gray-300 text-gray-500  dark:text-gray-50 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium")}
+                        >
                         Projects
                       </a>
                     </div>
@@ -123,10 +136,11 @@ export default function TopBar() {
                   Home
                 </Disclosure.Button>
                 <Disclosure.Button
+                  id="blogs"
                   // as={Link}
                   as="a"
                   href="/posts"
-                  className="border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 dark:text-gray-50 block pl-3 pr-4 py-2 border-l-4 text-base font-medium"
+                  className=" border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 dark:text-gray-50 block pl-3 pr-4 py-2 border-l-4 text-base font-medium"
                 >
                   Blogs
                 </Disclosure.Button>
