03b331167e3bdc71c1cd9cf1b98df8431e6c03ed
gkpk30 <guykoomer@hotmail.com>
2022-09-06 14:11:14 -0700

    added home  link to the logo

--- components/TopBar.js
+++ components/TopBar.js
@@ -107,7 +107,11 @@ export default function TopBar() {
                 <div className="flex justify-between " style={{ flex: "auto" }}>
                   <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
                     <div className="flex-shrink-0 flex items-center">
+                      <Link href="/">
+                      <a className="cursor-pointer">
                         {renderLogoChanger()}
+                        </a>
+                      </Link>
                      
                     </div>
 
